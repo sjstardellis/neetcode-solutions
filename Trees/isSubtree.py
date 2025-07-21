f9ffd8a5eb084a0bdb3e7c0267022c9efd1691f2
sstardellis <sjstardellis@gmail.com>
2025-07-21 18:22:35 -0400

    isSubtree solution O(M*N), could improve using serialization

---
+++ Trees/isSubtree.py
@@ -0,0 +1,56 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        stack = [root]
+
+        while stack:
+            # pop the top node of the stack
+            node = stack.pop()
+
+            # ensure node is not null
+            if node is None:
+                continue
+
+            # if the value of the node is the same as the subroot, check the rest of the tree
+            if node.val == subRoot.val:
+                # if true, then that subtree is in the main tree
+                if self.isSameTree(node, subRoot):
+                    return True
+
+            # if the children are not null, add them to the stack
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        return False
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # used for traversal, at the root nodes of both q and p
+        stack = [[p, q]]
+
+        # as long as there are nodes for traversal, continue loop
+        while stack:
+            # take off the top pair of nodes of the stack
+            node1, node2 = stack.pop()
+
+            # if they are not both null, continue
+            if not node1 and not node2:
+                continue
+
+            # if one of them is none/null or if not the same value, return false
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+
+            # add children to the stack
+            stack.append((node1.right, node2.right))
+            stack.append((node1.left, node2.left))
+        return True
